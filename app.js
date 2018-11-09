42b301a0e66f304acf925b19164cfdf0334ce136
Michael Tong <Michael_Tong@Outlook.com>
2018-11-09 12:52:05 -0600

    Updated port number to 80.

--- app.js
+++ app.js
@@ -162,8 +162,8 @@ app.get('/nextdrivingdoggo', function(req,res) {
 
 
 
-app.listen(3000, function(){
-	console.log('Server started on port 3000...');
+app.listen(80, function(){
+	console.log('Server started on port 80...');
 })
 
 
